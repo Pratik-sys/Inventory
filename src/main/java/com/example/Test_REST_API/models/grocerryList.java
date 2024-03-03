b9fc18571f6b5963bd8472889147fbb9d203e1c4
Pratik-sys <pithore.pratik@gmail.com>
2024-03-03 20:15:00 +0530

    Setup Entity

---
+++ src/main/java/com/example/Test_REST_API/models/grocerryList.java
@@ -0,0 +1,54 @@
+package com.example.Test_REST_API.models;
+
+import org.springframework.data.annotation.Id;
+
+
+public class grocerryList {
+    @Id
+
+    private  long id;
+    private String name;
+    private  String quantity;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    private  String category;
+
+    public grocerryList(long id, String name, String quantity, String category) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.category = category;
+    }
+
+}
