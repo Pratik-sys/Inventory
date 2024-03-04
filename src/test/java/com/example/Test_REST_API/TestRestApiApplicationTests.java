6110b20defb8ad2fa53e1ba4bfafa0200f7f39f0
Pratik-sys <pithore.pratik@gmail.com>
2024-03-04 20:08:25 +0530

    init project

---
+++ src/test/java/com/example/Test_REST_API/TestRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Test_REST_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
