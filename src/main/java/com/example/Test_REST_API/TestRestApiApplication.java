699288642ab2cabe61b56e931a4e05bd257ea09c
Pratik-sys <pithore.pratik@gmail.com>
2024-03-07 16:58:50 +0530

    remove commadline runner

--- src/main/java/com/example/Test_REST_API/TestRestApiApplication.java
+++ src/main/java/com/example/Test_REST_API/TestRestApiApplication.java
@@ -1,33 +1,12 @@
 package com.example.Test_REST_API;
-
-import com.example.Test_REST_API.model.GroceryItem;
-import com.example.Test_REST_API.repository.ItemRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
 
 @SpringBootApplication
-@EnableMongoRepositories
-public class TestRestApiApplication implements CommandLineRunner {
-	@Autowired
-	private ItemRepository groceryItemRepo;
+public class TestRestApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestRestApiApplication.class, args);
-
-	}
-
-	@Autowired
-	private  ItemRepository ip;
-	@Override
-	public void run(String... args) throws Exception {
-		GroceryItem gs = new GroceryItem();
-		gs.setName("Pratik");
-		gs.setQuantity("6");
-		gs.setCategory("testing");
-		ip.save(gs);
-
 
 	}
 }
