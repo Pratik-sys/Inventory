defe2772d3fabfb8a6a8e8b241a33d01c8322f4a
Pratik-sys <pithore.pratik@gmail.com>
2024-04-11 08:23:57 +0530

    change endpoint name and define request mapping

--- src/main/java/com/example/Inventory/controller/ProductController.java
+++ src/main/java/com/example/Inventory/controller/ProductController.java
@@ -7,24 +7,25 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("api/product")
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
-    @GetMapping("/items")
+    @GetMapping("/listAll")
     public ResponseEntity<List<Product>> getAllItems(){
         return  ResponseEntity.ok().body(productService.getAllGroceries());
     }
-    @PostMapping("/add-items")
+    @PostMapping("/add")
     public  ResponseEntity<Product> addItems(@RequestBody Product Product){
         return ResponseEntity.ok().body(productService.addItems(Product));
     }
-    @PutMapping("/item-update/{id}")
-    public ResponseEntity<Product> updateItems(@PathVariable("id") String id, @RequestBody Product Product) throws Exception {
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Product> updateItems(@PathVariable("id") String id, @RequestBody Product Product){
         return ResponseEntity.ok().body(productService.updateItem(id, Product));
     }
-    @DeleteMapping("/item-delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteItem(@PathVariable("id") String id) {
         productService.deleteItem(id);
         return ResponseEntity.ok().body("User successfully deleted!");
@@ -33,7 +34,7 @@ public class ProductController {
     public  ResponseEntity<List<Product>> getByCategory(@PathVariable("category") String category){
         return  ResponseEntity.ok().body(productService.finByCategory(category));
     }
-    @GetMapping("/stocks")
+    @GetMapping("/stock")
     public  ResponseEntity<List<Product>> getByAvailability(@RequestParam("stockAvailability") boolean stockAvailability){
         return  ResponseEntity.ok().body(productService.findByAvailability(stockAvailability));
     }
