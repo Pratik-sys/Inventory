5b7d2ef2d10507090d16197203a86f114056461d
Pratik-sys <pithore.pratik@gmail.com>
2024-04-19 11:10:45 +0530

    Remove @Query annonation from the extended repo

--- src/main/java/com/example/Inventory/repository/ProductRepository.java
+++ src/main/java/com/example/Inventory/repository/ProductRepository.java
@@ -1,14 +1,7 @@
 package com.example.Inventory.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import com.example.Inventory.model.Product;
-import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-public interface ProductRepository extends MongoRepository<Product, String>  {
-    @Query("{'category' : ?0}")
-    List<Product> findByCategory(String category);
-
-    @Query("{'stockAvailability' : ?0}")
-    List<Product> findByStockAvailability(boolean stockAvailability);
-}
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String>  {}
